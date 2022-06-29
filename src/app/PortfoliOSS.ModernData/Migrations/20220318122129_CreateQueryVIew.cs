b235aa46783778875f45d6e918b9cfd621bbef30
Sean Killeen <SeanKilleen@gmail.com>
2022-06-29 08:13:06 -0400

    Add some things I know are not sensitive

---
+++ src/app/PortfoliOSS.ModernData/Migrations/20220318122129_CreateQueryVIew.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfoliOSS.ModernData.Migrations
+{
+    public partial class CreateQueryVIew : Migration
+    {
+        public const string ORIGINAL_SQL =
+            @"select orgs.Name UserOrgName, users.UserId, users.Name UserName, prOrgs.Name OrgName, repos.Name RepoName, PullRequestId, CreatedOn, IsMerged, MergedDate 
+                    from PullRequests prs
+                    left outer join Users users on (prs.AuthorUserId = users.UserId)
+                    left outer join Repositories repos on (prs.RepositoryRepoId = repos.RepoId)
+                    left outer join Organizations prOrgs on (repos.OrganizationId = prOrgs.OrganizationId)
+                    left outer join OrganizationUser orgusers on (users.UserId = orgusers.UsersUserId)
+                    left outer join Organizations orgs on (orgusers.OrganizationsOrganizationId = orgs.OrganizationId)";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            const string CREATION_STATEMENT = $@"
+                CREATE VIEW PortfoliOSS as
+                 {ORIGINAL_SQL}";
+
+            migrationBuilder.Sql(CREATION_STATEMENT);
+        }
+    
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW PortfoliOSS");
+
+        }
+    }
+}
---
+++ src/app/PortfoliOSS.ModernData/Migrations/20220524200648_LimitViewToThoseAddedToAnOrg.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfoliOSS.ModernData.Migrations
+{
+    public partial class LimitViewToThoseAddedToAnOrg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                ALTER VIEW PortfoliOSS as
+                    select * from
+                    (
+                        select orgs.Name UserOrgName, users.UserId, users.Name UserName, prOrgs.Name OrgName, repos.Name RepoName, PullRequestId, CreatedOn, IsMerged, MergedDate 
+                        from PullRequests prs
+                        left outer join Users users on (prs.AuthorUserId = users.UserId)
+                        left outer join Repositories repos on (prs.RepositoryRepoId = repos.RepoId)
+                        left outer join Organizations prOrgs on (repos.OrganizationId = prOrgs.OrganizationId)
+                        left outer join OrganizationUser orgusers on (users.UserId = orgusers.UsersUserId)
+                        left outer join Organizations orgs on (orgusers.OrganizationsOrganizationId = orgs.OrganizationId)
+                    )Query
+                    WHERE Query.UserOrgName is not null
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                ALTER VIEW PortfoliOSS as
+                    {CreateQueryVIew.ORIGINAL_SQL}");
+        }
+    }
+}
