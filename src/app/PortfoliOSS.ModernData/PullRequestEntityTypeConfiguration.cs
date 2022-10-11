a844b43bd289b828a2aa709619345fc8a8f4dcff
Sean Killeen <SeanKilleen@gmail.com>
2022-10-11 09:29:27 -0400

    Extract files (#48)

---
+++ src/app/PortfoliOSS.ModernData/PullRequestEntityTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PortfoliOSS.ModernData;
+
+public class PullRequestEntityTypeConfiguration : IEntityTypeConfiguration<PullRequest>
+{
+    public void Configure(EntityTypeBuilder<PullRequest> builder)
+    {
+        builder.HasKey(x => new { x.PullRequestId, x.Repository.RepoId });
+    }
+}
---
+++ src/app/PortfoliOSS.ModernData/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PortfoliOSS.ModernData;
+
+public class Repository
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public long RepoId { get; set; }
+
+    public List<PullRequest> PullRequests { get; } = new();
+}
