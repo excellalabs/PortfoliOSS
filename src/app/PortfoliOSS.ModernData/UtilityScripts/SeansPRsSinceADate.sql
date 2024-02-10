bce84b81dfedcefd68971f5164c063a37cee814d
Sean Killeen <SeanKilleen@gmail.com>
2024-02-10 11:55:17 -0500

    utility script for my PRs

---
+++ src/app/PortfoliOSS.ModernData/UtilityScripts/SeansPRsSinceADate.sql
@@ -0,0 +1,8 @@
+﻿Select orgs.Name, repos.Name, MergedDate from PullRequests prs
+    left outer join Repositories repos on (prs.RepositoryRepoId = repos.RepoId)
+    left outer join Organizations orgs on (repos.OrganizationId = orgs.OrganizationId)
+    where AuthorUserId = (Select UserId from Users where Name ='SeanKilleen') 
+    and orgs.Name != 'SeanKilleen'
+    and IsMerged = 1
+    and MergedDate >= '2023-03-01'
+    order by MergedDate
